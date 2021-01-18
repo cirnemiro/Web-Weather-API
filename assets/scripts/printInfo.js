b6023efd38104fc53018ca3e84cfbe770fa4c744
IGNACIOFC <45830117+IGNACIOFC@users.noreply.github.com>
2021-01-18 17:22:19 +0100

    current day section

--- assets/scripts/printInfo.js
+++ assets/scripts/printInfo.js
@@ -1,8 +1,13 @@
+
 import { data } from './request.js'
 
 export const printCurrentDay = (day=0)=>{
-    console.log(data);
-
+    $(".currentDay__title").html(document.querySelector('.header_content_search__input').value);
+    
+    console.log(data.daily[0].weather[0].description);
+    $(".currentDay__hour").html(data.daily[0].dt);
+    $(".temperature").html(Math.round(data.daily[0].temp.day - 273.15) + "º")
+    $(".weather").html(data.daily[0].weather[0].description)
 }
 
 export const printNextdays = ()=>{
