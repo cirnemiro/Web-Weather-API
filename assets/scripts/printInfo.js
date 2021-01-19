5679bd33e252b2d91e9380d07a201aedc65afdb8
cirnemiro <cirnemiro@gmail.com>
2021-01-19 11:28:39 +0100

    google api

--- assets/scripts/printInfo.js
+++ assets/scripts/printInfo.js
@@ -1,13 +1,12 @@
 
 import { data } from './request.js'
 
-export const printCurrentDay = (day=0)=>{
-
-    $(".currentDay__title").html(document.querySelector('.header_content_search__input').value);
-    
-    console.log(data);
+export const printCurrentDay = (cityName,day=0)=>{
+    console.log('city name', cityName);
+    $(".currentDay__title").html(cityName);
+    console.log(data.daily[0].weather[0].description);
     $(".currentDay__hour").html(data.daily[0].dt);
-    $(".temperature").html(Math.round(data.current.temp - 273.15) + "º")
+    $(".temperature").html(Math.round(data.daily[0].temp.day - 273.15) + "º")
     $(".weather").html(data.daily[0].weather[0].description)
 
     
